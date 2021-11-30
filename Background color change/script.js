173b929f4f40185ace394dd6871fddbf789bf7b1
PedroLucasLopes <pedrolopesparaguai@gmail.com>
2021-11-29 22:45:16 -0300

    Background color project second commit

--- Background color change/script.js
+++ Background color change/script.js
@@ -1,8 +1,14 @@
+
+
 function change() {
     const btn = document.querySelector('body')
+    const colorPicker = document.querySelector('footer p')
+
     const red = Math.floor(Math.random() * 256)
     const green = Math.floor(Math.random() * 256)
     const blue = Math.floor(Math.random() * 256)
 
     btn.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
+
+    colorPicker.textContent = `CSS: rgb(${red}, ${green}, ${blue})`
 }
